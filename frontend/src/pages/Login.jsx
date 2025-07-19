457c44c01007a0b7d0ce4bae7fb11020d9fc3be9
harshpatidar245 <harshpatidar2400@gmail.com>
2025-07-19 14:21:33 +0530

    login VITE IN ENV

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
 
   const { login } = useAuth();
   const navigate = useNavigate();
-
+  const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
   // Timer countdown for OTP resend
   useEffect(() => {
     let interval = null;
@@ -74,7 +74,7 @@ const Login = () => {
       setError("");
       setSuccess("");
 
-      const response = await axios.post(`${VITE_BACKEND_URL}/api/auth/send-email-otp`, { email });
+      const response = await axios.post(`${API_BASE_URL}/api/auth/send-email-otp`, { email });
 
       if (response.data.success) {
         setOtpSent(true);
